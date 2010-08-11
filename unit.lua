63bccfe261cc28069c93192f7520c52a7a32932f
Nick Barendt <nickbarendt@gmail.com>
2010-08-11 08:33:08 -0400

    made test discovery/runner application exit with a non-zero status code when any test fails

--- unit.lua
+++ unit.lua
@@ -1,5 +1,6 @@
 #! /usr/bin/env lua
 require "unittest"
+require "os"
 
 local result = unittest.TestResult {}
 local test_files = unittest.findTestFiles()
@@ -8,6 +9,7 @@ suite:run(result)
 print(result:summary())
 if not result:status() then
     print(unittest.FailureReporter{result}:report())
+    os.exit(1)
 else
     print("OK")
 end
